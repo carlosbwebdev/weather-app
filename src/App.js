0bc673188aa50d603c27f9ab1c9be1b5ce6951d1
Carlos Barros <carlosbwebdev@gmail.com>
2022-02-07 17:56:21 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -11,4 +11,11 @@ function App() {
   );
 }
 
+/*
+
+
+https://api.openweathermap.org/data/2.5/onecall?lat=38.9954378&lon=-9.1411938&exclude=current&minutely&appid=901da5ef428d43a8b4abb585d4aa1406
+
+*/
+
 export default App;
